4e49d20fbfb1f8cc8f07ad036c1ad1cc193d6f46
maria93101 <maria93101@gmail.com>
2017-03-05 20:31:18 -0800

    Just checking if my username was changed

--- player.cpp
+++ player.cpp
@@ -52,6 +52,7 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * TODO: Implement how moves your AI should play here. You should first
      * process the opponent's opponents move before calculating your own move
      */
+     //hello
      
      // Initialize a vector with all 64 moves
      vector<Move> legal_moves;
